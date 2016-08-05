07dc47bd1bfca0b28820da9e3d1db326748b3014
Richard Giddings <rgiddings20@hotmail.com>
2016-08-05 11:34:31 +0100

    - Styling changes. - Made Tricks inline in the admin section.

--- tips/admin.py
+++ tips/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Section, Tip, Trick
 
 admin.site.register(Section)
-admin.site.register(Tip)
-admin.site.register(Trick)
+
+class TrickInline(admin.StackedInline):
+    model = Trick
+    extra = 2
+
+class TipAdmin(admin.ModelAdmin):
+    inlines = [TrickInline]
+
+admin.site.register(Tip, TipAdmin)
